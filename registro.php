db16b7f80637a3cff60a49cdc429bd94f8b11f07
Martinezadrian91 <47789717+Martinezadrian91@users.noreply.github.com>
2025-07-12 15:06:06 -0300

    Add files via upload

---
+++ registro.php
@@ -0,0 +1,14 @@
+<?php
+$ip = $_SERVER['REMOTE_ADDR'];
+$fecha = date("Y-m-d H:i:s");
+
+$archivo = "visitas.txt";
+
+if (file_put_contents($archivo, "$fecha - $ip\n", FILE_APPEND)) {
+    http_response_code(200);
+    echo "OK";
+} else {
+    http_response_code(500);
+    echo "Error al guardar";
+}
+?>
